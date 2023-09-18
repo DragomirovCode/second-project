6631f0a8334b19505538eb322111e3031d0fce65
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2023-09-19 00:07:21 +0300

    release v3

--- src/main/java/org/example/config/SpringConfig.java
+++ src/main/java/org/example/config/SpringConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -14,6 +16,7 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 /* нужные зависимости: "Spring Core", "Spring Context", "Spring MVC", "Thymeleaf" */
 
     /*
@@ -30,13 +33,17 @@ import javax.sql.DataSource;
     его подпакеты для поиска и создания бинов (компонентов). */
 
 @EnableWebMvc /*  Эта аннотация активирует конфигурацию Spring MVC, включая обработку HTTP запросов и представлений. */
+@PropertySource("classpath:database.properties") // путь к бд
 public class SpringConfig implements WebMvcConfigurer  {
 
     private final ApplicationContext applicationContext;
 
+    private final Environment environment;
+
     @Autowired
-    public SpringConfig(ApplicationContext applicationContext) {
+    public SpringConfig(ApplicationContext applicationContext, Environment environment) {
         this.applicationContext = applicationContext;
+        this.environment = environment;
     }
 
     @Bean
@@ -68,10 +75,10 @@ public class SpringConfig implements WebMvcConfigurer  {
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5433/first_bd");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("Sdsafcxvre2341");
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("driver")));
+        dataSource.setUrl(environment.getProperty("url"));
+        dataSource.setUsername(environment.getProperty("user"));
+        dataSource.setPassword(environment.getProperty("password"));
         return dataSource;
     }
 
@@ -80,19 +87,3 @@ public class SpringConfig implements WebMvcConfigurer  {
         return new JdbcTemplate(dataSource());
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
